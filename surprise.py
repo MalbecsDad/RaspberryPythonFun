1414b886b7e41f4d471baa4b15dcc71f65eb5da3
MalbecsDad <vibbizz@yahoo.co.uk>
2021-06-14 10:45:01 +0100

    Add files via upload

---
+++ surprise.py
@@ -0,0 +1,20 @@
+from sense_hat import SenseHat
+from random import randint
+sense = SenseHat()
+
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+white = (255, 255, 255)
+yellow = (255, 255, 0)
+
+while True:
+
+    # Generate a random colour
+    def pick_random_colour():
+        random_red = randint(0, 255)
+        random_green = randint(0, 255)
+        random_blue = randint(0, 255)
+        return (random_red, random_green, random_blue)
+
+    sense.show_message("Happy Father's Day!", text_colour=pick_random_colour(), back_colour=pick_random_colour())
